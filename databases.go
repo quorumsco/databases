78f7fd2ae050b6fa3ba7e73e8b40de60f4cf6bd4
Luka <luka@blow.sh>
2018-09-28 11:21:51 +0200

    Cleaning sources following coding style
    Switched package variables to constants (with proper comments):
       - timeout (sleep duration between each connection retry)
       - retry (number of times to retry)
    
    Updated some comments, and factorizing variables declarations.

--- databases.go
+++ databases.go
@@ -4,41 +4,42 @@ package databases
 import (
 	"time"
 
-	"github.com/quorumsco/logs"
 	"github.com/jinzhu/gorm"
 	"github.com/jmoiron/sqlx"
+	"github.com/quorumsco/logs"
 
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-var (
-	//TIMEOUT time between each try
-	TIMEOUT = 5 * time.Second
-	//RETRY number of tries
-	RETRY = 3
-)
+// timeout defines the sleep duration between each connection retry.
+const timeout = 5 * time.Second
+
+// retry defines the number of times to retry.
+const retry = 3
 
 // Return a sqlx client
 func InitSQLX(dialect, args string) (*sqlx.DB, error) {
-	var db *sqlx.DB
-	var err error
+	var (
+		db  *sqlx.DB
+		err error
+		i   int
+	)
 
-	var i int
 retry:
 	for {
 		db, err = sqlx.Connect(dialect, args)
 		switch {
 		case err == nil:
 			break retry
-		case i >= RETRY:
+		case i >= retry:
 			return nil, err
 		default:
 			logs.Error(err)
 			i++
 		}
-		time.Sleep(TIMEOUT)
+		time.Sleep(timeout)
 	}
 
 	if db.Ping() != nil {
@@ -48,25 +49,35 @@ retry:
 	return db, nil
 }
 
-// Return a gorm client
-func InitGORM(dialect, args string) (*gorm.DB, error) {
-	var db *gorm.DB
-	var err error
+/*
+InitGORM returns a gorm client.
 
-	var i int
+It wraps gorm.Open(dialect, args ...interface{}) in order to allows retrying connection on failure. Example:
+
+	db, err := InitGORM("postgres", "postgres://user:password@/dbname?charset=utf8&parseTime=True&loc=Local")
+
+Number of retries depends on the `retry` package constant, separated by a `timeout` time constant.
+*/
+func InitGORM(dialect, args string) (*gorm.DB, error) {
+	var (
+		db  *gorm.DB
+		err error
+		i   int
+	)
+
 retry:
 	for {
 		db, err = gorm.Open(dialect, args)
 		switch {
 		case err == nil:
 			break retry
-		case i >= RETRY:
+		case i >= retry:
 			return nil, err
 		default:
 			logs.Error(err)
 			i++
 		}
-		time.Sleep(TIMEOUT)
+		time.Sleep(timeout)
 	}
 
 	if db.DB().Ping() != nil {
